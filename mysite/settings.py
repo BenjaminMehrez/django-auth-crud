2b9287ca05beff35bc2ec7fdabed8b3124826e7b
BenjaminMehrez <benjaminjofre01@gmail.com>
2024-08-15 21:54:03 -0300

    csrf

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,3 +144,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ALLOWED_HOSTS = ['localhost','django-auth-crud-production.up.railway.app']
+
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://django-auth-crud-production.up.railway.app']
