ccba90c2b29b441471cd234c28acf03718aa994e
BenjaminMehrez <benjaminjofre01@gmail.com>
2024-08-16 18:19:59 -0300

    nose

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-ALLOWED_HOSTS = ['localhost','django-auth-crud-production.up.railway.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','django-auth-crud-production.up.railway.app']
 
 
 CSRF_TRUSTED_ORIGINS = ['http://*','https://django-auth-crud-production.up.railway.app']
